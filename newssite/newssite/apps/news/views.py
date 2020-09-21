e716883accc68bb41d52501d2d68ee9abf68bdf4
Thomas Lazarus <tlazarus@ksu.edu>
2020-09-20 21:57:24 -0500

    Setup Pycharm Debugger and some minor bootstrap

--- newssite/newssite/apps/news/views.py
+++ newssite/newssite/apps/news/views.py
@@ -8,10 +8,12 @@ class NewsList(TemplateView):
     site_articles = {}
 
     def __init__(self):
+        super().__init__()
         self.s = Scraper()
+        self.s.scrape_all_articles()
 
     def get_context_data(self, **kwargs):
-        self.site_articles = self.s.get_all_site_articles()
+        self.site_articles = self.s.get_all_site_articles()['verge']
         context = super().get_context_data(**kwargs)
         context['arts'] = self.site_articles
         return context
