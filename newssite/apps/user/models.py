915593f95dd179361986788cdea4f22f07f7b937
Thomas Lazarus <tlazarus@ksu.edu>
2020-10-03 18:29:10 -0500

    Removes db service since sqlite3 will be used

--- newssite/apps/user/models.py
+++ newssite/apps/user/models.py
@@ -5,3 +5,6 @@ class User(models.Model):
     full_name = models.CharField(max_length=50)
     email = models.CharField(max_length=25, unique=True)
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.email
