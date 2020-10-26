4d32a6fc08c2bc591025d5261d925da40b7ae592
Thomas Lazarus <tlazarus@ksu.edu>
2020-10-26 15:44:36 -0500

    Adds map functionality
    I don't think this is working as intended. I think it is currently just shaving off maybe a few seconds per article. I also added comments to the web_scraper.py file
    Sets PySpark Master

--- newssite/apps/summarizer/web_scraper.py
+++ newssite/apps/summarizer/web_scraper.py
@@ -8,12 +8,12 @@ from pyspark import SparkConf, SparkContext
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 
-# conf = SparkConf().setAppName("Collinear Points")
-# sc = SparkContext('local', conf=conf)
-# gateway = JavaGateway()
-
-
 class Scraper:
+    """
+    This is the class to handle web scraping. It is able to read in and summarize news articles from preset rss feeds.
+    """
+
+    # link_dict is a dictionary of links where the key is a given news site's slug and the value is the rss feed's url
     link_dict = {
         'verge': 'https://www.theverge.com/rss/index.xml',
         # 'nyTimes_US': 'https://rss.nytimes.com/services/xml/rss/nyt/US.xml',
@@ -21,11 +21,13 @@ class Scraper:
         # 'cnet': 'https://www.cnet.com/rss/news/',
     }
 
+    # individual dictionaries to store a given news site's articles, headlines, and links
     verge_dict = {}
     nyTime_dict = {}
     wired_dict = {}
     cnet_dict = {}
 
+    # linking dictionary from slug to article dictionary
     article_to_dict = {
         'verge': verge_dict,
         # 'nyTimes_US': nyTime_dict,
@@ -33,16 +35,29 @@ class Scraper:
         # 'cnet': cnet_dict,
     }
 
+    # list of all articles
     articles = []
 
+    # list of links that are frequent in rss feed but that don't need to be scraped
     not_allowed_urls = ['https://www.nytimes.com', 'https://www.nytimes.com/section/us', 'https://www.wired.com',
                         'https://www.cnet.com/#ftag=CAD590a51e']
 
+    # tokenizer for text summarization
     tokenizer = AutoTokenizer.from_pretrained("google/pegasus-cnn_dailymail", use_fast=True)
     # Make sure the file is unzipped
+    # model for text summarization
     model = AutoModelForSeq2SeqLM.from_pretrained("apps/summarizer/model", local_files_only=True)
 
+    # Sets up PySpark Things
+    conf = SparkConf().setAppName("Web App").setMaster('spark://spark-master:7077')
+    SparkContext.setSystemProperty('spark.python.worker.memory', '2g')
+    sc = SparkContext('local', conf=conf)
+    gateway = JavaGateway
+
     def scrape_all_articles(self):
+        """
+        Scrapes in all articles from rss feeds in link_dict
+        """
         i = 0
         for site in self.link_dict:
             print(f'Started Scraping {site}')
@@ -54,6 +69,7 @@ class Scraper:
             soup = BeautifulSoup(res.text, 'xml')
 
             for art in soup.findAll('link')[1:]:
+                articles = []
                 if site != 'verge':
                     for x in art:
                         art_link = x
@@ -64,33 +80,51 @@ class Scraper:
                         article = Article(art_link)
                         article.download()
                         article.parse()
-                        # self.articles.append(article.text)
-                        self.article_to_dict[site][article.title] = {'link': art_link, 'article_loc': self.summarize((article.text))}
+                        articles.append(article.text)
+                        self.article_to_dict[site][article.title] = {'link': art_link, 'article_loc': i}
                         i += 1
                     except:
                         print(f'ERROR: {art_link}')
 
-            # arts_text = sc.parallelize(self.articles)
-            # arts_text.foreach(self.summarize)
+                # Creates PySpark RDD and saves it in cache. Then maps the summarize function
+                arts_text = sc.parallelize(articles)
+                arts_text.cache()
+                arts_map = arts_text.map(lambda z: self.summarize(z))
+                self.articles.append(arts_map.collect())
             print(f'Finished Scraping {site}')
-        # self.update_articles_in_dict()
+        # Replaces the index value in article_to_dict[site][article.title][article_loc] to the summarized article string
+        self.update_articles_in_dict()
 
     def get_specific_site_articles(self, slug=None):
+        """
+        Takes in a slug and returns the summarized articles for just that slug
+        TODO: Remove the method if it is not needed
+        """
         if slug:
             return self.article_to_dict[slug]
         else:
             print("ERROR: Please Enter Slug")
 
     def get_all_site_articles(self):
+        """
+        Returns all the summarized articles
+        """
         return self.article_to_dict
 
     def get_all_site_slugs(self):
+        """
+        Returns a list of all the scraped site slugs
+        TODO: Remove the method if it is not needed
+        """
         slug_list = []
         for slug in self.link_dict:
             slug_list.append(slug)
         return slug_list
 
     def update_articles_in_dict(self):
+        """
+        Replaces index with article text in dictionary
+        """
         for site, headline_dict in self.article_to_dict.items():
             for headline, link_dict in headline_dict.items():
                 if isinstance(link_dict['article_loc'], int):
@@ -99,6 +133,9 @@ class Scraper:
                     })
 
     def summarize(self, art):
+        """
+        Summarizes the passed in article text
+        """
         print("Start Summarizing")
         start_time = time.time()
         batch = self.tokenizer.prepare_seq2seq_batch([art], max_target_length=100)
