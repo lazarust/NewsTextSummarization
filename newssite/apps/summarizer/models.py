9496b9ab4c1604fb2ba62ce39198b04e0680052c
Thomas Lazarus <tlazarus@ksu.edu>
2021-01-06 21:23:40 -0600

    Adds article and site models

--- newssite/apps/summarizer/models.py
+++ newssite/apps/summarizer/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Class to handle each site
+class Site(models.Model):
+    pass
+
+
+# Class to handle each article
+class Article(models.Model):
+    date = models.DateField()
+    site = models.ManyToManyField(Site, blank=False)
+    summary = models.TextField()
+    article_link = models.TextField()
+    headline = models.TextField()
+
+    class Meta(object):
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+
+    def __str__(self):
+        return self.headline + " " + str(self.date)
