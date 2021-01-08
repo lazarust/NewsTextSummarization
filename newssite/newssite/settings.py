8e0e0d5b945499e6b8e72e3983aa9ac71ebfbda0
Thomas Lazarus <tlazarus@ksu.edu>
2021-01-07 19:43:18 -0600

    Regenerate secret key and move it to env

--- newssite/newssite/settings.py
+++ newssite/newssite/settings.py
@@ -9,14 +9,14 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import environ
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+root = environ.Path(__file__) - 3  # three folder back (/a/b/c/ - 3 = /)
+BASE_DIR = Path(root())
+env = environ.Env()
+env.read_env(str(BASE_DIR.joinpath('.env')))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
