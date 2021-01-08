11a908eda71eb111a638c5a8e3a4dc8f1d9c8be1
Thomas Lazarus <tlazarus@ksu.edu>
2021-01-07 19:19:28 -0600

    Fixes cloudbuild

--- newssite/newssite/settings.py
+++ newssite/newssite/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*%4z&^wk@w^kr18n@a&xn^#6x0p6kd&y&uxpt$u)i#$&u68a^b'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
