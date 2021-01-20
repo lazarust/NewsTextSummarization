4cdd97450faeaa11552d518fd2ff830230fdc6f4
Thomas Lazarus <tlazarus@ksu.edu>
2021-01-19 22:23:39 -0600

    Initial Migrations

--- newssite/apps/summarizer/models.py
+++ newssite/apps/summarizer/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 
+
 # Class to handle each site
 class Site(models.Model):
-    pass
+    name = models.TextField(null=False, blank=True)
+    slug = models.SlugField(null=False, blank=True)
+    url_feed = models.TextField(null=False, blank=True)
+
+    class Meta(object):
+        verbose_name = "Site"
+        verbose_name_plural = "Sites"
 
 
 # Class to handle each article
 class Article(models.Model):
     date = models.DateField()
-    site = models.ManyToManyField(Site, blank=False)
+    site = models.ForeignKey(Site, blank=True, null=True, on_delete=models.SET_NULL)
     summary = models.TextField()
     article_link = models.TextField()
     headline = models.TextField()
