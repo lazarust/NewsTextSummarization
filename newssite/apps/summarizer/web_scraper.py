a59d6505150f57e49a34d9cd15546eebebe93eaa
Thomas Lazarus <tlazarus@ksu.edu>
2021-01-27 19:23:39 -0600

    Fixes for using that DB

--- newssite/apps/summarizer/web_scraper.py
+++ newssite/apps/summarizer/web_scraper.py
@@ -34,13 +34,12 @@ class Scraper:
         """
         start_scrape = time.time()
         bulk_articles = []
-        for site in self.link_dict:
-            articles = []
+        for site in self.sites:
             print(f'Started Scraping {site}')
-            link = self.link_dict[site]
+            link = site.url_feed
             res = requests.get(link)
             if res.status_code == 404:
-                self.article_to_dict[site]["ERROR"] = "RSS feed responded with 404"
+                print(f"RSS feed responded with 404: {site}")
 
             soup = BeautifulSoup(res.text, 'xml')
             scraped_links = soup.findAll('link')[1:]
@@ -48,7 +47,7 @@ class Scraper:
                 scraped_links = scraped_links[:10]
             for art in scraped_links:
                 art_link = ""
-                if site != 'TheVerge':
+                if site.slug != 'the-verge':
                     for x in art:
                         art_link = x
                 else:
@@ -59,27 +58,23 @@ class Scraper:
                         article.download()
                         article.parse()
                         bulk_articles.append(Article(date=datetime.now(), site=site, summary=self.summarize(article.text), article_link=art_link, headline=article.title))
-                        articles.append(article.text)
                     except:
                         print(f'ERROR: {art_link}')
 
             print(f'Finished Scraping {site}')
             Article.objects.bulk_create(bulk_articles)
-            print(f'Summarizing {site}')
-            self.summarize(articles)
-            print(f'Finished Summarizing {site}')
         end_scrape = time.time()
         total_time = end_scrape - start_scrape
         print(f'TOTAL TIME: {total_time}')
 
-    def summarize(self, arts):
+    def summarize(self, art):
         """
         Summarizes the passed in article text
         """
         print("Start")
         start_time = time.time()
-        tgt_text = self.pipeline(arts)
+        tgt_texts = self.pipeline(art)
         end_time = time.time()
         time_diff = end_time - start_time
         print(f'Finish TIME: {time_diff}')
-        return tgt_text
+        return tgt_texts
